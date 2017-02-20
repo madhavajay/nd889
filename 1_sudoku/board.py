9dc6af5eba9fd33947cb76f8edffba01470e1afc
Madhava Jay <me@madhavajay.com>
2017-02-20 10:33:47 +0100

    Initial sudoku solver code base with tests

---
+++ 1_sudoku/board.py
@@ -0,0 +1,210 @@
+# -*- coding: utf-8 -*-
+# Author: github.com/madhavajay
+"""This is a sudoku board"""
+
+from typing import List, Dict, Set, TYPE_CHECKING
+
+
+class Board:
+    values = '123456789'
+    rows = 'ABCDEFGHI'
+    row_squares = ['ABC', 'DEF', 'GHI']
+    cols = '123456789'
+    col_squares = ['123', '456', '789']
+    _peers = None  # type: Dict[str, Set[str]]
+    _all_units = None  # type: List[List[str]]
+
+    @staticmethod
+    def cross(x: str, y: str) -> List[str]:
+        return [a+b for a in x for b in y]
+
+    @staticmethod
+    def boxes() -> List[str]:
+        return Board.cross(Board.rows, Board.cols)
+
+    @staticmethod
+    def num_boxes() -> int:
+        return len(Board.rows) * len(Board.cols)
+
+    @staticmethod
+    def row_units() -> List[List[str]]:
+        return [Board.cross(r, Board.cols) for r in Board.rows]
+
+    @staticmethod
+    def column_units() -> List[List[str]]:
+        return [Board.cross(Board.rows, c) for c in Board.cols]
+
+    @staticmethod
+    def square_units() -> List[List[str]]:
+        return [Board.cross(rs, cs)
+                for rs in (Board.row_squares)
+                for cs in (Board.col_squares)]
+
+    @staticmethod
+    def all_units() -> List[List[str]]:
+        if Board._all_units is None:
+            Board._all_units = (Board.row_units() +
+                                Board.column_units() +
+                                Board.square_units())
+        return Board._all_units
+
+    @staticmethod
+    def generate_peers() -> Dict[str, Set[str]]:
+        boxes = Board.boxes()
+
+        # add all combinations of units together
+        all = Board.all_units()
+
+        # re-arrange to dictionary of boxes and array of their units
+        units = dict(
+            (box, [unit for unit in all if box in unit]) for box in boxes)
+        # flatten into a dictionary of boxes and arrays of their peer boxes
+        peers = dict(
+            (box, set(sum(units[box], [])) - set([box])) for box in boxes)
+
+        return peers
+
+    @staticmethod
+    def peers(box: str) -> Set[str]:
+        if Board._peers is None:
+            Board._peers = Board.generate_peers()
+        return Board._peers[box]
+
+    @staticmethod
+    def grid_values(board_string: str) -> Dict[str, str]:
+        if len(board_string) != Board.num_boxes():
+            raise ValueError('Board string length must be 81 characters')
+
+        board_dict = {}
+        boxes = Board.boxes()
+        for index, value in enumerate(board_string):
+            if value == '.':
+                value = Board.values
+            board_dict[boxes[index]] = value
+        return board_dict
+
+    @staticmethod
+    def board_to_str(board_dict: Dict[str, str]) -> str:
+        """
+        Convert a board dictionary into a comma separated string of values
+        in order of the original boxes array for easy comparison during tests
+        """
+        string = ''
+        for key in Board.boxes():
+            string += board_dict[key] + ','
+        return string[:-1]
+
+    @staticmethod
+    def eliminate(board_dict: Dict[str, str]) -> Dict[str, str]:
+        solved_values = [box for box in board_dict.keys()
+                         if len(board_dict[box]) == 1]
+        for box in solved_values:
+            digit = board_dict[box]
+            for peer in Board.peers(box):
+                board_dict[peer] = board_dict[peer].replace(digit, '')
+        return board_dict
+
+    @staticmethod
+    def only_choice(board_dict: Dict[str, str]) -> Dict[str, str]:
+        all = Board.all_units()
+        for unit in all:
+            for number in range(1, 10):
+                possible_boxes = [box for box in unit
+                                  if str(number) in board_dict[box]]
+                if len(possible_boxes) == 1:
+                    board_dict[possible_boxes[0]] = str(number)
+        return board_dict
+
+    @staticmethod
+    def num_solved_boxes(board_dict: Dict[str, str]) -> int:
+        return len([box for box in board_dict.keys()
+                    if len(board_dict[box]) == 1])
+
+    @staticmethod
+    def sorted_box_possibilities(board_dict: Dict[str, str]) -> List[str]:
+        options = [box for box in board_dict.keys()
+                   if len(board_dict[box]) > 1]
+        return sorted(options, key=lambda box: len(board_dict[box]))
+
+    @staticmethod
+    def reduce_puzzle(board_dict: Dict[str, str]) -> Dict[str, str]:
+        stalled = False
+        while not stalled:
+            # Check how many boxes have a determined value
+            solved_values_before = Board.num_solved_boxes(board_dict)
+
+            # Use the Eliminate Strategy
+            board_dict = Board.eliminate(board_dict)
+
+            # Use the Only Choice Strategy
+            board_dict = Board.only_choice(board_dict)
+
+            # Check how many boxes have a determined value, to compare
+            solved_values_after = Board.num_solved_boxes(board_dict)
+
+            # If no new values were added, stop the loop.
+            stalled = solved_values_before == solved_values_after
+
+            # If no values at all terminate and return None
+            if solved_values_after == 0:
+                return None
+        return board_dict
+
+    @staticmethod
+    def validate(board_dict: Dict[str, str]) -> bool:
+        valid = True
+        all = Board.all_units()
+        complete_unit = set(Board.values)
+        for unit in all:
+            unit_values = [board_dict[box] for box in unit]
+            unit_set = set(unit_values)
+            if unit_set != complete_unit:
+                print('bad set {}'.format(unit_set))
+                valid = False
+                break
+        return valid
+
+    @staticmethod
+    def search(board_dict: Dict[str, str]) -> Dict[str, str]:
+        """
+        Using depth-first search and propagation,
+        create a search tree and solve the sudoku.
+        """
+        # First, reduce the puzzle using the previous function
+        reduced = Board.reduce_puzzle(board_dict)
+        if reduced is None:
+            return reduced
+
+        if Board.num_solved_boxes(reduced) == Board.num_boxes():
+            if Board.validate(reduced):
+                return reduced
+            else:
+                return None
+
+        # Choose one of the unfilled squares with the fewest possibilities
+        possibilities = Board.sorted_box_possibilities(reduced)
+        if len(possibilities) > 0:
+            smallest_box = possibilities[0]
+            choices = list(reduced[smallest_box])
+
+            for combination in choices:
+                board_copy = reduced.copy()
+                board_copy[smallest_box] = combination
+                result = Board.search(board_copy)
+                if result is not None:
+                    return result
+
+        return None
+
+    @staticmethod
+    def display(values: Dict[str, str]) -> None:
+        width = 1 + max(len(values[s]) for s in Board.boxes())
+        line = '+'.join(['-' * (width * 3)] * 3)
+        for r in Board.rows:
+            print(''.join(
+                values[r + c].center(width) +
+                ('|' if c in '36' else '')
+                for c in Board.cols)
+            )
+            if r in 'CF':
+                print(line)
--- README.md
+++ README.md
@@ -1,2 +1,25 @@
 # nd889
 Udacity Artificial Intelligence Nanodegree
+
+## Setup
+This code uses the following:
+- python 3.6
+- [pylint](http://www.pylint.org) &amp; PEP 8 - Style Guide
+- [mypy](http://mypy-lang.org) &amp; PEP 484 - Type Hints
+- [pipenv](http://pipenv.org) &amp; PEP 508 - Dependency spec
+- [pytest](http://pytest.org) - Tests
+
+Install pipenv:
+```bash
+$ pip install pipenv
+```
+
+Initialize pipenv in python3 mode:
+```bash
+$ pipenv --three
+```
+
+Run tests:
+```bash
+$ pytest
+```
