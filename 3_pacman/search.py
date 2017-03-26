bdcfdf61eba9ba1ca56dec79d5d710a2c76612ac
Madhava Jay <me@madhavajay.com>
2017-03-26 10:18:45 +0200

    Completed Pacman Questions 1 - 6

--- 3_pacman/search.py
+++ 3_pacman/search.py
@@ -12,6 +12,61 @@ by Pacman agents (in searchAgents.py).
 """
 
 import util
+from util import Stack, Queue, PriorityQueue, PriorityQueueWithFunction
+from util import manhattanDistance
+
+from typing import NamedTuple, Tuple, List, Any
+
+
+class Node:
+  def __init__(self, state: Tuple[int, int], action: str, cost: int, parent: Any):
+    self.state = state
+    self.action = action
+    if parent:
+      self.cost = cost + parent.cost
+    else:
+      self.cost = 0
+    self.parent = parent
+
+  def parent_state(self):
+    if self.parent:
+      return self.parent.state
+    return None
+
+  def get_path(self):
+    path = []
+    node = self
+    while node.parent_state():
+      path.append(node.action)
+      node = node.parent
+
+    return list(reversed(path))
+
+  def __eq__(self, other):
+    if not isinstance(other, Node):
+        return NotImplemented
+    return (
+      self.state == other.state and
+      self.action == other.action and
+      self.cost == other.cost and
+      self.parent_state() == other.parent_state()
+    )
+
+  def __ne__(self, other):
+    return not self.__eq__(other)
+
+  def __lt__(self, other):
+    return self.cost < other.cost
+
+  def __gt__(self, other):
+    return self.cost > other.cost
+
+  def __hash__(self):
+    return hash(tuple(sorted(self.__dict__.items())))
+
+  def __str__(self):
+    return f'{self.state} {self.action} {self.cost} {self.parent_state()}'
+
 
 class SearchProblem:
   """
@@ -67,6 +122,28 @@ def tinyMazeSearch(problem):
   w = Directions.WEST
   return  [s,s,w,s,w,w,s,w]
 
+
+def graph_search(problem, frontier) -> List[str]:
+  explored = set()
+  while not frontier.isEmpty():
+    node = frontier.pop()
+    if problem.isGoalState(node.state):
+      return node.get_path()
+    if node.state not in explored:
+      explored.add(node.state)
+      children = problem.getSuccessors(node.state)
+      for child in children:
+        child_node = Node(child[0], child[1], child[2], node)
+        if child_node.state not in explored:
+          if isinstance(frontier, PriorityQueueWithFunction):
+            frontier.push(child_node)
+          elif isinstance(frontier, PriorityQueue):
+            frontier.push(child_node, child_node.cost)
+          else:
+            frontier.push(child_node)
+
+  return []
+
 def depthFirstSearch(problem):
   """
   Search the deepest nodes in the search tree first
@@ -83,21 +160,39 @@ def depthFirstSearch(problem):
   print "Is the start a goal?", problem.isGoalState(problem.getStartState())
   print "Start's successors:", problem.getSuccessors(problem.getStartState())
   """
-  "*** YOUR CODE HERE ***"
-  util.raiseNotDefined()
+
+  node = Node(problem.getStartState(), None, 0, None)
+  if problem.isGoalState(node.state):
+    return node.get_path()
+
+  frontier = Stack()
+  frontier.push(node)
+  result = graph_search(problem, frontier)
+  return result
 
 def breadthFirstSearch(problem):
   """
   Search the shallowest nodes in the search tree first.
   [2nd Edition: p 73, 3rd Edition: p 82]
   """
-  "*** YOUR CODE HERE ***"
-  util.raiseNotDefined()
-      
+  node = Node(problem.getStartState(), None, 0, None)
+  if problem.isGoalState(node.state):
+    return node.get_path()
+
+  frontier = Queue()
+  frontier.push(node)
+  return graph_search(problem, frontier)
+
 def uniformCostSearch(problem):
   "Search the node of least total cost first. "
-  "*** YOUR CODE HERE ***"
-  util.raiseNotDefined()
+
+  node = Node(problem.getStartState(), None, 0, None)
+  if problem.isGoalState(node.state):
+    return node.get_path()
+
+  frontier = PriorityQueue()
+  frontier.push(node, 0)
+  return graph_search(problem, frontier)
 
 def nullHeuristic(state, problem=None):
   """
@@ -106,11 +201,22 @@ def nullHeuristic(state, problem=None):
   """
   return 0
 
+def manhattanHeuristic(state, problem=None):
+  return manhattanDistance(state, problem.goal)
+
 def aStarSearch(problem, heuristic=nullHeuristic):
   "Search the node that has the lowest combined cost and heuristic first."
-  "*** YOUR CODE HERE ***"
-  util.raiseNotDefined()
-    
+  node = Node(problem.getStartState(), None, 0, None)
+  if problem.isGoalState(node.state):
+    return node.get_path()
+
+  def wrapper(node):
+    return heuristic(node.state, problem)
+
+  frontier = PriorityQueueWithFunction(wrapper)
+  frontier.push(node)
+  return graph_search(problem, frontier)
+
   
 # Abbreviations
 bfs = breadthFirstSearch
--- 3_pacman/searchAgents.py
+++ 3_pacman/searchAgents.py
@@ -35,6 +35,7 @@ import util
 import time
 import search
 import searchAgents
+from util import manhattanDistance
 
 class GoWestAgent(Agent):
   "An agent that goes West until it can't."
@@ -275,50 +276,39 @@ class CornersProblem(search.SearchProblem):
       if not startingGameState.hasFood(*corner):
         print(('Warning: no food in corner ' + str(corner)))
     self._expanded = 0 # Number of search nodes expanded
-    
-    "*** YOUR CODE HERE ***"
+
     
   def getStartState(self):
     "Returns the start state (in your state space, not the full Pacman state space)"
-    "*** YOUR CODE HERE ***"
-    util.raiseNotDefined()
+    return (self.startingPosition, self.corners)
     
   def isGoalState(self, state):
     "Returns whether this search state is a goal state of the problem"
-    "*** YOUR CODE HERE ***"
-    util.raiseNotDefined()
-       
+
+    loc, goals = state
+    if len(goals) == 0:
+      return True
+    return False
+
+
   def getSuccessors(self, state):
-    """
-    Returns successor states, the actions they require, and a cost of 1.
-    
-     As noted in search.py:
-         For a given state, this should return a list of triples, 
-     (successor, action, stepCost), where 'successor' is a 
-     successor to the current state, 'action' is the action
-     required to get there, and 'stepCost' is the incremental 
-     cost of expanding to that successor
-    """
-    
     successors = []
     for action in [Directions.NORTH, Directions.SOUTH, Directions.EAST, Directions.WEST]:
-      # Add a successor state to the successor list if the action is legal
-      # Here's a code snippet for figuring out whether a new position hits a wall:
-      #   x,y = currentPosition
-      #   dx, dy = Actions.directionToVector(action)
-      #   nextx, nexty = int(x + dx), int(y + dy)
-      #   hitsWall = self.walls[nextx][nexty]
-      
-      "*** YOUR CODE HERE ***"
-      
+      old_loc, goals = state
+      x, y = old_loc
+      dx, dy = Actions.directionToVector(action)
+      action_x, action_y = int(x + dx), int(y + dy)
+      new_loc = (action_x, action_y)
+      if not self.walls[action_x][action_y]:
+        if new_loc in goals:
+          goals = set(goals) - {new_loc}
+        new_state = (new_loc, tuple(goals))
+        successors.append((new_state, action, 1))
+
     self._expanded += 1
     return successors
 
   def getCostOfActions(self, actions):
-    """
-    Returns the cost of a particular sequence of actions.  If those actions
-    include an illegal move, return 999999.  This is implemented for you.
-    """
     if actions == None: return 999999
     x,y= self.startingPosition
     for action in actions:
@@ -327,25 +317,45 @@ class CornersProblem(search.SearchProblem):
       if self.walls[x][y]: return 999999
     return len(actions)
 
-
 def cornersHeuristic(state, problem):
   """
   A heuristic for the CornersProblem that you defined.
-  
-    state:   The current search state 
+    state:   The current search state
              (a data structure you chose in your search problem)
-    
-    problem: The CornersProblem instance for this layout.  
-    
+    problem: The CornersProblem instance for this layout.
   This function should always return a number that is a lower bound
   on the shortest path from the state to a goal of the problem; i.e.
   it should be admissible (as well as consistent).
   """
   corners = problem.corners # These are the corner coordinates
   walls = problem.walls # These are the walls of the maze, as a Grid (game.py)
-  
-  "*** YOUR CODE HERE ***"
-  return 0 # Default to trivial solution
+
+  top = problem.corners[1][1]
+  right = problem.corners[2][0]
+  loc, goals_left = state
+
+  # if at goal state return 0
+  if loc in corners:
+    return 0
+
+  goals_hit = set(corners) - set(goals_left)
+  goal_count = len(goals_hit)
+  value = 0
+
+  if goal_count == 0:
+    value = top + right + min(top, right)
+
+  elif goal_count == 1:
+    value = top + right
+
+  distances = []
+  for goal in goals_left:
+    distances.append(manhattanDistance(loc, goal))
+  if len(distances):
+    value += min(distances)
+
+  return value
+
 
 class AStarCornersAgent(SearchAgent):
   "A SearchAgent for FoodSearchProblem using A* and your foodHeuristic"
